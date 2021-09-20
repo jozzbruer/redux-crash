7c7a22e974252a2cd49ef8aee1f047c525ddd690
Joz-Bruer Quince <quincejozbruer@gmail.com>
2021-09-20 16:05:33 -0700

    Creating all the files to setup redux in the app

---
+++ src/state/reducers/accountReducer.js
@@ -0,0 +1,13 @@
+// read the action to return the appropriate state
+const reducer = (state = 0, action) => {
+	switch (action.type) {
+		case 'deposit':
+			return state + action.payload;
+		case 'withdraw':
+			return state - action.payload;
+		default:
+			return state;
+	}
+};
+
+export default reducer;
